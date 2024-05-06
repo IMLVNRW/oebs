ed2da4ff46e0a28ff10f2a59a262616fe508f662
IMLVNRW <webadmin.sec.56465189@im-nrw.de>
2024-05-06 15:31:00 +0200

    create login

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Album example for Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/album/">
+
+    <script src="assets/js/jquery-3.7.1.min.js" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link href="assets/css/custom.css" rel="stylesheet">
+  </head>
---
+++ login.php
@@ -0,0 +1,63 @@
+<?php 
+require_once('config.php');
+
+if(isset($_GET['login'])) {
+    $errorMessage = user_cheack_login($_POST['username'], $_POST['password']);
+}
+
+
+?>
+
+
+
+<?php include_once('includes/header.php') ?>
+
+  <body>
+    <header>
+        <?php include_once('includes/nav.php') ?>
+    </header>
+
+  <main role="main">
+    <section class="jumbotron text-center">
+        <div class="container">
+          <h1 class="jumbotron-heading">Open EBS </h1>
+          <p class="lead text-muted">Elektronisches Beratung System</p>
+        </div>
+    </section>
+        
+    <div class="login-content ">
+        <div class="card">
+            <div class="card-body py-5 px-md-5">
+                <form action="?login=1" id="login-form" method="post">
+                    
+                <?php 
+                    if(isset($errorMessage)) {
+                        echo $errorMessage;
+                    }
+                    ?>
+                    <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="text" name="username" id="username"  class="form-control form-control-lg />
+                    <label class="form-label" for="username">Email address</label>
+                    </div>
+
+                    <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="password" name="password" id="password" class="form-control form-control-lg" required="required">   
+                    <label class="form-label" for="password">Password</label>
+                    </div>
+            
+                    <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">
+                    Login
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php include_once('includes/footer.php') ?>
+</body>
+
+
+</html>
+
+                
